146753fe91e5cf18b9a64dabc66adcd43abc03de
mrthomas20121 <thomas77300@gmail.com>
2020-03-18 21:58:36 +0100

    More Pokedex work

--- api/pokedex.js
+++ api/pokedex.js
@@ -6,6 +6,7 @@ class Pokedex {
    * @param {Number} n the pokedex to look at(ex: 2 is kanto, 3 is hoeen, etc...)
    */
   constructor(n) {
+    this.id = n;
     this.url = `https://pokeapi.co/api/v2/pokedex/${n}`;
 
   }
@@ -15,15 +16,29 @@ class Pokedex {
    */
   async getPokemonByName(name) {
     return await fetch(this.url).then(res => res.json()).then(data => data.pokemon_entries.find((arr) => arr.pokemon_species.name === name))
-
   }
   /**
    * Get Pokemon By Id
    * @param {Number} id 
    */
   async getPokemonById(id) {
-    let res = await fetch(this.url).then(res => res.json()).then(data => data.pokemon_entries[id])
-    return res
+    return await fetch(this.url).then(res => res.json()).then(data => data.pokemon_entries[id])
+  }
+  async getRegionName() {
+    return await fetch(this.url).then(res => res.json()).then(data => data.name)
+  }
+  /**
+   * Get the Description
+   * @param {String} lang the language code.
+   */
+  async getDescription(lang='en') {
+    return await fetch(this.url).then(res => res.json()).then(data => data.descriptions.find((d) => d.language.name === lang ))
+  }
+  /**
+   * Return the Pokedex Id
+   */
+  getId() {
+    return this.id;
   }
 }
 
@@ -32,6 +47,8 @@ async function test() {
   const dex = new Pokedex(2);
   //let res = await dex.getPokemonById(0);
   let res = await dex.getPokemonByName('bulbasaur');
-  console.log(res);
+
+  let desc = await dex.getDescription();
+  console.log(desc, res)
 }
 test()
