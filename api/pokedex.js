b834a5d0472e8325588521155fbedf3a47f118f5
mrthomas20121 <thomas77300@gmail.com>
2020-03-18 16:02:41 +0100

    Fixed Await crash

--- api/pokedex.js
+++ api/pokedex.js
@@ -6,26 +6,32 @@ class Pokedex {
    * @param {Number} n the pokedex to look at(ex: 2 is kanto, 3 is hoeen, etc...)
    */
   constructor(n) {
-    fetch(`https://pokeapi.co/api/v2/pokedex/${n}`).then(res => res.json()).then(data => this.data = data)
+    this.url = `https://pokeapi.co/api/v2/pokedex/${n}`;
+
   }
   /**
    * get a pokemon by name
    * @param {String} name the pokemon name
    */
-  getPokemonByName(name) {
-    return this.data
+  async getPokemonByName(name) {
+    return await fetch(this.url).then(res => res.json()).then(data => data.pokemon_entries.find((arr) => arr.pokemon_species.name === name))
 
   }
   /**
    * Get Pokemon By Id
    * @param {Number} id 
    */
-  getPokemonById(id) {
-    return this.data
+  async getPokemonById(id) {
+    let res = await fetch(this.url).then(res => res.json()).then(data => data.pokemon_entries[id])
+    return res
   }
 }
 
-
-const dex = new Pokedex(2);
-let res = dex.getPokemonById(0);
-console.log(res);
+// Tests
+async function test() {
+  const dex = new Pokedex(2);
+  //let res = await dex.getPokemonById(0);
+  let res = await dex.getPokemonByName('bulbasaur');
+  console.log(res);
+}
+test()
